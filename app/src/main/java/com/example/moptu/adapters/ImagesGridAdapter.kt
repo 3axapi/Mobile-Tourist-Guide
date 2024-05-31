36810619db22983f8404328c7ac8aa64a0b8a0a4
zakhar.bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2024-05-31 20:34:47 +0200

    Improve everything of GridView in AdviserList

---
+++ app/src/main/java/com/example/moptu/adapters/ImagesGridAdapter.kt
@@ -0,0 +1,77 @@
+package com.example.moptu.adapters
+
+import android.content.Context
+import android.graphics.Typeface
+import android.media.Image
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.app.NotificationCompat.Style
+import androidx.core.view.marginBottom
+import androidx.core.view.setMargins
+import com.example.moptu.R
+import com.example.moptu.model.Places
+import kotlinx.coroutines.newFixedThreadPoolContext
+
+class ImagesGridAdapter (
+    private val context: Context,
+    private val titlesList: List<String>,
+    private val imagesList: List<Int>
+    ) : BaseAdapter() {
+    override fun getCount(): Int = imagesList.size
+
+    override fun getItem(position: Int): Any = imagesList[position]
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val linearLayout: LinearLayout
+        val imageView: ImageView
+        val textView: TextView
+
+        if (convertView == null) {
+            linearLayout = LinearLayout(context).apply {
+                orientation = LinearLayout.HORIZONTAL
+
+                val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                ).apply { setMargins(0, 0, 0, 25) }
+                this.layoutParams = params
+            }
+
+            imageView = ImageView(context).apply {
+                layoutParams = LinearLayout.LayoutParams(450, 350).also {
+                    it.setMargins(8, 8, 8, 8)
+                }
+                scaleType = ImageView.ScaleType.CENTER_CROP
+            }
+
+            textView = TextView(context).apply {
+                layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT
+                )
+                text = titlesList[position]
+                textAlignment = View.TEXT_ALIGNMENT_CENTER
+                setTextSize(20f)
+                setTypeface(null, Typeface.BOLD)
+                gravity = Gravity.CENTER
+            }
+
+            linearLayout.addView(imageView)
+            linearLayout.addView(textView)
+        } else {
+            linearLayout = convertView as LinearLayout
+            imageView = linearLayout.getChildAt(0) as ImageView
+        }
+
+        imageView.setImageResource(imagesList[position])
+
+        return linearLayout
+    }
+}
--- app/src/main/java/com/example/moptu/ui/fragments/AdviserListFragment.kt
+++ app/src/main/java/com/example/moptu/ui/fragments/AdviserListFragment.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridView
 import androidx.fragment.app.Fragment
 import com.example.moptu.R
+import com.example.moptu.adapters.ImagesGridAdapter
+import com.example.moptu.model.Places
 
 class AdviserListFragment : Fragment() {
+    lateinit var gridView: GridView
+    lateinit var listOfPlaces: List<Places.Place>
+    lateinit var listOfTitles: List<String>
+    lateinit var listOfImages: List<Int>
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,5 +25,15 @@ class AdviserListFragment : Fragment() {
             R.layout.fragment_adviser_list,
             container, false
         )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        listOfPlaces = Places.getPlaces()
+        listOfTitles = listOfPlaces.map { it.title }
+        listOfImages = listOfPlaces.map { it.imageResId }
+        gridView = view.findViewById(R.id.gridView_places)
+        gridView.adapter = ImagesGridAdapter(requireContext(), listOfTitles, listOfImages)
     }
 }
