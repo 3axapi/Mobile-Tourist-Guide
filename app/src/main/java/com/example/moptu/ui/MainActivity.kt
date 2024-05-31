589d26fdf648c95c2e1308e3e4011c5169e7c670
zakhar.bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2024-05-31 14:29:06 +0200

    Initialize new Android application and Add xml fragments with kotlin Fragments

---
+++ app/src/main/java/com/example/moptu/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moptu.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moptu.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/moptu/ui/fragments/WelcomeFragment.kt
@@ -0,0 +1,7 @@
+package com.example.moptu.ui.fragments
+
+import androidx.fragment.app.Fragment
+
+class WelcomeFragment : Fragment() {
+
+}
